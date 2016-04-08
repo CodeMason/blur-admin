7f54cc92373568d2cda45d623b401c2be7287c8d
alex <demeshko.alexander.v@gmail.com>
2016-04-08 22:23:06 +0300

    fail to fix

--- src/app/theme/components/baPanel/baPanelBlur.directive.js
+++ src/app/theme/components/baPanel/baPanelBlur.directive.js
@@ -24,6 +24,7 @@
       restrict: 'A',
       link: function($scope, elem) {
         if(!$rootScope.$isMobile) {
+          elem.append('<div class="bg"></div>')
           baPanelBlurHelper.bodyBgLoad().then(function () {
             setTimeout(recalculatePanelStyle);
           });
@@ -38,10 +39,30 @@
           if (!bodyBgSize) {
             return;
           }
-          elem.css({
+          elem.children('.bg').css({
             backgroundSize: Math.round(bodyBgSize.width) + 'px ' + Math.round(bodyBgSize.height) + 'px',
-            backgroundPosition: Math.floor(bodyBgSize.positionX) + 'px ' + Math.floor(bodyBgSize.positionY) + 'px'
+            backgroundPosition: Math.floor(bodyBgSize.positionX) + 'px ' + Math.floor(bodyBgSize.positionY) + 'px',
+            width: Math.round(bodyBgSize.width)+ 'px ',
+            height: Math.round(bodyBgSize.height)+ 'px ',
+            top: Math.floor(bodyBgSize.positionY) + 'px ',
+            left: Math.floor(bodyBgSize.positionX) + 'px '
           });
+
+
+          //elem.children('style').remove();
+          //var style = "<style>";
+          //var id = '_' + Math.random().toString(36).substr(2, 9);
+          //elem.attr('id', id);
+          //var backgroundSize = Math.round(bodyBgSize.width) + 'px ' + Math.round(bodyBgSize.height) + 'px';
+          //var backgroundPosition = Math.floor(bodyBgSize.positionX) + 'px ' + Math.floor(bodyBgSize.positionY) + 'px';
+          //elem.append('<style>'
+          //  + '#' + id + '::before{background-size:' + backgroundSize + ';'
+          //  + 'width:' + Math.round(bodyBgSize.width).toString() + 'px;'
+          //  + 'height:' + Math.round(bodyBgSize.height).toString() + 'px;'
+          //  + 'top:' + Math.floor(bodyBgSize.positionY).toString() + 'px;'
+          //  + 'left:' + Math.floor(bodyBgSize.positionX).toString() + 'px;'
+          //  + '}'
+          //  + '</style>')
         }
 
       }
